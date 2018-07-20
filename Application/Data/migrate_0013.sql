860c5748e4e03477f0b705315446def4cd09c040
wanghe <wanghemix@163.com>
2018-07-20 21:34:19 +0800

    修改初中可选学校

---
+++ Application/Data/migrate_0013.sql
@@ -0,0 +1,19 @@
+DELETE FROM `t_lookup` where type = 'MIDDLE_SCHOOL';
+
+INSERT INTO t_lookup(type,name,val,seq_num)
+value
+('MIDDLE_SCHOOL', '1', '枣庄41中南校', 1),
+('MIDDLE_SCHOOL', '2', '枣庄41中北校', 2),
+('MIDDLE_SCHOOL', '3', '龙子心中学', 3),
+('MIDDLE_SCHOOL', '4', '齐村中学', 4),
+('MIDDLE_SCHOOL', '5', '渴口中学', 5),
+('MIDDLE_SCHOOL', '6', '孟庄中学', 6),
+('MIDDLE_SCHOOL', '7', '周村中学', 7),
+('MIDDLE_SCHOOL', '8', '安城一贯制', 8),
+('MIDDLE_SCHOOL', '9', '枣庄26中', 9),
+('MIDDLE_SCHOOL', '10', '枣庄13中', 10),
+('MIDDLE_SCHOOL', '11', '西王庄中学', 11),
+('MIDDLE_SCHOOL', '12', '枣庄42中', 12),
+('MIDDLE_SCHOOL', '13', '枣庄15中东校', 13),
+('MIDDLE_SCHOOL', '13', '枣庄15中西校', 14)
+;
